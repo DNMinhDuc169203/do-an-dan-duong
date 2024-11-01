a8e3a7dd08740a2fbfdaac28945f518f90582ddf
DNMinhDuc169203 <dangducmtp@gmail.com>
2024-11-01 10:58:36 +0700

    demo2

--- app/src/main/java/stu/edu/vn/Dangnhap.java
+++ app/src/main/java/stu/edu/vn/Dangnhap.java
@@ -1,25 +1,35 @@
 package stu.edu.vn;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Dangnhap extends AppCompatActivity {
 
     EditText txtEmail, txtpass;
-    Button dangky;
+    Button btndangnhap;
     FirebaseAuth mAuth;
 
     public void addControls(){
         txtEmail =findViewById(R.id.txtEmail);
+        txtpass =findViewById(R.id.txtPassword);
+        btndangnhap =findViewById(R.id.btndangnhap);
+        mAuth =  FirebaseAuth.getInstance();
     }
 
     @Override
@@ -31,6 +41,36 @@ public class Dangnhap extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        addControls();
+
+        btndangnhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = txtEmail.getText().toString().trim();
+                String password = txtpass.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(Dangnhap.this, "Vui lòng nhập đầy đủ thông tin!", Toast.LENGTH_SHORT).show();
+                } else {
+                    dang_Nhap_User(email, password);
+                }
+
+            }
+        });
+    }
+
+    private void dang_Nhap_User(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(Dangnhap.this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
+                    // Chuyển sang màn hình chính của ứng dụng
+                } else {
+                    Toast.makeText(Dangnhap.this, "Đăng nhập thất bại: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
--- app/src/main/java/stu/edu/vn/dangky.java
+++ app/src/main/java/stu/edu/vn/dangky.java
@@ -1,25 +1,35 @@
 package stu.edu.vn;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class dangky extends AppCompatActivity {
 
-    private EditText etEmail, etPassword;
-    private Button btnRegister;
+    private EditText txtEmail, txtPassword;
+    private Button btnDangKy;
     private FirebaseAuth mAuth;
 
     public void addControls(){
-
+    txtEmail = findViewById(R.id.txtEmail);
+    txtPassword = findViewById(R.id.txtPassword);
+    btnDangKy = findViewById(R.id.btnDangKy);
+    mAuth = FirebaseAuth.getInstance();
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +40,37 @@ public class dangky extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        addControls();
+
+        btnDangKy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = txtEmail.getText().toString().trim();
+                String password = txtPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)){
+                    Toast.makeText(dangky.this,"Vui long nhap thong tin day du", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    dang_Ky_user(email,password);
+                }
+            }
+        });
+    }
+
+    private void dang_Ky_user(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(dangky.this, "Dang Ky thanh cong", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(dangky.this, "Dang ky that bai" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
